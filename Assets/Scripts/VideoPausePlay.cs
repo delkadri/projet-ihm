c0ef5b8992ff7ecb2342b933c2e0963f1687258c
RBinda <raphaelbinda78960@gmail.com>
2026-01-09 16:06:03 +0100

    wip: add video

---
+++ Assets/Scripts/VideoPausePlay.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPausePlay : MonoBehaviour
+{
+    private VideoPlayer player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<VideoPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangePausePlay()
+    {
+        if (player.isPlaying)
+        {
+            // play pause sound
+            player.Pause();
+        }
+        else
+        {
+            // play play sound
+            player.Play();
+        }
+    }
+}
